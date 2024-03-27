ec6336a4b2cf7aa3833500a72f04472b0134c0f5
netrolite <netrolite@proton.me>
2024-03-27 14:20:24 +0300

    remove code

--- src/main/java/com/netrolite/tnthailstorm/TntSpawner.java
+++ src/main/java/com/netrolite/tnthailstorm/TntSpawner.java
@@ -3,51 +3,36 @@ package com.netrolite.tnthailstorm;
 import com.netrolite.tnthailstorm.random.RandNum;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.TntEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.network.packet.s2c.play.TitleS2CPacket;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
 
 public class TntSpawner {
   private static final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-  private static int ticks = 0;
+  private static int tickCounter = 0;
   private static RandNum rand = new RandNum();
-  private static int startHailstormAfterTicks = genHailstormAfterTicks();
-  private static boolean hasWarned = false;
+  private static int startHailstormAfterTicks = getHailstormWaitTimeTicks();
 
   public static void spawnTntOnInterval() {
     ServerTickEvents.END_WORLD_TICK.register(
         (world) -> {
-          ticks++;
-          warnAboutHailstorm(world);
-          if (ticks < startHailstormAfterTicks) return;
-          // reset some variables on hailstorm start
-          ticks = 0;
-          hasWarned = false;
-          startHailstormAfterTicks = genHailstormAfterTicks();
-
-          System.out.println("will start hailstorm after " + startHailstormAfterTicks + " ticks");
-
+          tickCounter++;
+          if (tickCounter < startHailstormAfterTicks) return;
+          tickCounter = 0;
+          startHailstormAfterTicks = getHailstormWaitTimeTicks();
           ArrayList<String> playersToSkip = new ArrayList<>();
-          System.out.println("getPlayers(world) " + getPlayers(world));
-          System.out.println("playersToSkip " + playersToSkip);
 
           getPlayers(world)
               .forEach(
                   (player) -> {
-                    System.out.println("playersToSkip " + playersToSkip);
-                    System.out.println("curPlayer " + player.getUuidAsString());
-                    System.out.println(
-                        "will skip? " + playersToSkip.contains(player.getUuidAsString()));
                     if (playersToSkip.contains(player.getUuidAsString())) return;
 
                     PlayerEntity closestPlayer = null;
@@ -63,18 +48,13 @@ public class TntSpawner {
                       }
                     }
 
-                    System.out.println("closestPlayer " + closestPlayer);
-                    System.out.println("closestPlayerDistance " + closestPlayerDistance);
-
                     if (closestPlayerDistance < 20) {
-                      System.out.println("closest player is close enough");
                       playersToSkip.add(closestPlayer.getUuidAsString());
                     }
 
-                    for (int j = 0; j < 40; j++) {
+                    for (int j = 0; j < 100; j++) {
                       scheduler.schedule(
                           () -> {
-                            System.out.println("scheduling tnt callback");
                             BlockPos playerPos = player.getBlockPos();
 
                             int tntSpawnOffsetX = rand.genInt(-10, 10);
@@ -92,12 +72,10 @@ public class TntSpawner {
                             while (!canSpawnTnt) {
                               tntSpawnPos = tntSpawnPos.up(1);
                               canSpawnTnt = world.getBlockState(tntSpawnPos).isAir();
-                              System.out.println("canSpawnTnt " + canSpawnTnt);
                             }
-                            System.out.println("canSpawnTnt " + canSpawnTnt);
 
                             final BlockPos finalTntSpawnPos = tntSpawnPos;
-                            Entity tnt = new TntEntity(EntityType.TNT, world);
+                            Entity tnt = new CustomTntEntity(EntityType.TNT, world);
 
                             tnt.setPos(
                                 (double) finalTntSpawnPos.getX(),
@@ -107,11 +85,9 @@ public class TntSpawner {
                                 rand.genFloat((float) -1.5, (float) 1.5),
                                 rand.genFloat((float) -1, (float) -0.1),
                                 rand.genFloat((float) -1.5, (float) 1.5));
-                            System.out.println("tnt before spawning " + tnt);
-                            System.out.println("isClient before spawning " + world.isClient());
+                            tnt.setUuid(UUID.randomUUID());
 
-                            boolean spawnResult = world.spawnEntity(tnt);
-                            System.out.println("spawnResult " + spawnResult);
+                            world.spawnEntity(tnt);
                           },
                           rand.genInt(0, 20),
                           TimeUnit.SECONDS);
@@ -121,24 +97,8 @@ public class TntSpawner {
         });
   }
 
-  private static int genHailstormAfterTicks() {
-    return rand.genInt(2400, 3600);
-  }
-
-  private static void warnAboutHailstorm(ServerWorld world) {
-    System.out.println("warnAboutHailstorm:");
-    System.out.println("ticks " + ticks);
-    System.out.println("startHailstormAfterTicks " + startHailstormAfterTicks);
-    if (hasWarned) return;
-    if (ticks >= (startHailstormAfterTicks - 80)) {
-      List<ServerPlayerEntity> players = getPlayers(world);
-      players.forEach(
-          player -> {
-            player.networkHandler.sendPacket(
-                new TitleS2CPacket(Text.translatable("hailstorm_approaching")));
-          });
-      hasWarned = true;
-    }
+  private static int getHailstormWaitTimeTicks() {
+    return rand.genInt(800, 1200);
   }
 
   private static List<ServerPlayerEntity> getPlayers(ServerWorld world) {
--- src/main/java/com/netrolite/tnthailstorm/Tnthailstorm.java
+++ src/main/java/com/netrolite/tnthailstorm/Tnthailstorm.java
@@ -1,6 +1,5 @@
 package com.netrolite.tnthailstorm;
 
-import com.netrolite.tnthailstorm.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,7 +10,6 @@ public class TntHailstorm implements ModInitializer {
 
   @Override
   public void onInitialize() {
-    ModItems.registerModItems();
     TntSpawner.spawnTntOnInterval();
   }
 }
